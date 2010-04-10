97f5747ae46a464956dc20961800dc50e63e97e2
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-04-10 17:54:39 -0300

    fixed dependency version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/paraseba/faker"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :repositories [["clojars" "http://clojars.org/repo"]])
